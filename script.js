0fe75bba494111cf8dd6150bd6596022b9b51b44
Yuvan100 <86732878+Yuvan100@users.noreply.github.com>
2021-11-12 15:52:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+document.getElementsByTagName("h2")[0].style.fontSize = "60";
+document.getElementsByTagName("h3")[0].style.fontSize = "6vw";
+document.getElementsByTagName("h4")[0].style.fontSize = "6vw";
